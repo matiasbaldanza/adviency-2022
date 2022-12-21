41c9e9cc6f3188a199ac033ad2714d0afb997fa5
Matias Baldanza <matiasbaldanza@gmail.com>
2022-12-21 13:35:25 -0300

    FEATURE: When adding an existing element, the quantity is incremented by the quantity in the form. TODO: The list is not updated, and thus does not reflect the state in the storage

--- day-08/script.js
+++ day-08/script.js
@@ -19,35 +19,31 @@ const msgEmptyList = document.querySelector("#msg-empty");
 form.addEventListener("submit", e => {
     e.preventDefault();
     const newGiftName = giftName.value;
-    const newGiftQty = giftQty.value;
+    const newGiftQty = parseInt(giftQty.value);
     
      // VALIDATION: If text is empty, notify user
     if (newGiftName.trim() === "") {
         applyAnimation(form, "shake", 500);
-    } else /* if (giftIndex(newGift) === -1) */ {
+    } else {
         // Hide the Empty List message if adding the first element
         if (isGiftListEmpty()) hideMsgEmptyList();
 
         // Add gift to list
-        addItem(newGiftName, newGiftQty); 
-        storeItem(newGiftName, newGiftQty);
+        showItem(newGiftName, newGiftQty);      
+        storeItem(newGiftName, newGiftQty);     // Handles append or increment
 
         // Clear the form and return focus to first input
         form.reset(); 
         form.firstElementChild.focus({focusVisible: true});
-
-    } /* else {
-        // Notify user that gift already exists
-        showMsgGiftAlreadyExists(giftIndex(newGift));
-    } */
+    } 
     
 } );
 
 function giftIndex(gift) {
     // comparison strips additional whitespace and takes into account locale versions of characters using localeCompare
     return listOfGifts.findIndex(element => 
-        normalizeString(element)
-            .localeCompare(normalizeString(gift), 
+        cleanWhitespace(element)
+            .localeCompare(cleanWhitespace(gift), 
                             'default', { sensitivity: 'base', ignorepunctuation: true }) === 0
         ); 
 }
@@ -90,12 +86,19 @@ btnDeleteAll.addEventListener("click", (event) => {
 
 // UTILITIES
 
-const normalizeString = (string) => { 
+const cleanWhitespace = (string) => { 
     return string
             .toLowerCase()
             .trim()
             .replace(/\s+/g, ' ');
 };
+
+const stringsEqual = (string1, string2) => {
+    return cleanWhitespace(string1)
+            .localeCompare(cleanWhitespace(string2), 
+                            'default', { sensitivity: 'base', ignorepunctuation: true }) 
+            === 0;
+}
 
 const applyAnimation = (element, animationClass, timeout) => {
     element.classList.add(animationClass);
@@ -105,24 +108,36 @@ const applyAnimation = (element, animationClass, timeout) => {
 // FUNCTIONS
 
 function storeItem(itemName, itemQty = 1) {
-    listOfGifts.set(itemName, itemQty);
+    let found = false;
+    listOfGifts.forEach( (value, key) => {
+        // If found, increment the quantity
+        if (stringsEqual(key, itemName)) {
+            found = true;
+            listOfGifts.set(key, value + itemQty);
+        }
+    } );    
+
+    // If not found, append to the list
+    if (!found) { 
+        listOfGifts.set(itemName, itemQty);
+    }
 }
 
 function deleteItemFromStorage(itemName) {
     listOfGifts.delete(itemName);
 }
 
-function addItem(itemName, itemQty = 1) {
+function showItem(itemName, itemQty = 1) {
     // add the element to the page with its name
     // append a <span> to include additional info (quantity)
     // the new element is animated in CSS
+    
     const newElement = document.createElement('li');
     newElement.textContent = `${itemName}`;
     
     const newElementSpan = document.createElement('span');
     newElementSpan.classList.add('gift-info');
     newElementSpan.textContent = ` (${itemQty})`;
-
 
     newElement.appendChild(newElementSpan);
 
@@ -142,15 +157,15 @@ function hideMsgEmptyList() {
     msgEmptyList.classList.add("hidden");
 }
 
-function initializeList() {
+function showList() {
     if (isGiftListEmpty()) { 
         showMsgEmptyList();
     } else {
-        listOfGifts.forEach((giftQty, giftName) => { addItem(giftName, giftQty) });
+        listOfGifts.forEach((giftQty, giftName) => { showItem(giftName, giftQty) });
     }
 }
 
-initializeList();
+showList();
 
 /* 
 
